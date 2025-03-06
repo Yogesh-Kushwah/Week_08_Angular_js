59c140a6835d6f81e72667a5e9e53e0d2c057e44
unknown <yogeshkushwah775@gmail.com>
2025-03-06 12:48:29 +0530

    [Yogesh kushwah]Add. Hello from Bridgelabz with logo and  data binding  angularjs program

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,17 +1,17 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'Hello World  ';
+export class AppComponent { 
+  title: string = " Yogesh AngularJS"; 
+  ImageURL = "https://gumlet.assettype.com/freepressjournal%2F2021-06%2F32af7f9f-b603-4e12-8340-ba2425bb5ee8%2FBridgeLabz.PNG"; 
+  url: string = "https://www.bridgelabz.com";
 
+  ngOnInit(): void {
+    this.title = "Yogesh, Hello from BridgeLabz";  
+  }
 
-ngOnInit():void{
-  this.title="Hello from Bridgelabz.";
-}
 }
